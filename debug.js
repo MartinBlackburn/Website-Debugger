3184235ad65963bc1d6294714570764e1f9ff3d1
Martin Blackburn <walking_fishy@hotmail.com>
2012-10-30 12:36:06 +0000

    added debug bar, with click to toggle errors and error counts

--- debug.js
+++ debug.js
@@ -1,52 +1,125 @@
 Debug = function(element) 
 {
-	//run all checks
+	/*----------------------------------------------------------------------------------------------------------------*\
+    	DISPLAY DEBUG BAR
+	\*----------------------------------------------------------------------------------------------------------------*/
+	$('<div/>', {
+	    "class": "debugger",
+	    "style": "padding: 20px; " +
+	    		"border: 1px solid #ccc; " +
+	    		"box-shadow: 1px 1px 5px #000; " +
+	    		"background: #fff; " +
+	    		"color: #333; " +
+	    		"position: fixed; " +
+	    		"top: 0; " +
+	    		"left: 0; " +
+	    		"z-index: 9999;"
+	}).prependTo("body");
+	
+	$('<h1/>', {
+		"style": "margin-bottom: 10px;",
+	    "text": "Debug bar"
+	}).prependTo(".debugger");
+	
+	$('<div/>', {
+		"style": "margin-bottom: 10px;",
+	    "text": "Click to hide / show errors and warnings"
+	}).appendTo(".debugger");
+	
+	$('<div/>', {
+		"style": "cursor: pointer;",
+		"class": "imageMissingAlt",
+		click: function() {
+			imageMissingAlt();
+		}
+	}).appendTo(".debugger");
+	
+	$('<div/>', {
+		"style": "cursor: pointer;",
+		"class": "imageEmptyAlt",
+		click: function() {
+			imageEmptyAlt();
+		}
+	}).appendTo(".debugger");
+	
+	
+	
+	
+	
+	/*----------------------------------------------------------------------------------------------------------------*\
+		SET COUNTS TO 0
+	\*----------------------------------------------------------------------------------------------------------------*/
+	var imageMissingAltCount = 0;
+	var imageEmptyAltCount = 0;
+	
+	
+	
+	
+	/*----------------------------------------------------------------------------------------------------------------*\
+		RUN ALL CHECKS
+	\*----------------------------------------------------------------------------------------------------------------*/
 	imageMissingAlt();
-	imageEmptyAlt()
+	imageEmptyAlt();
 	
 	
 	
 	
 	
-	//toggle error on an element
+	/*----------------------------------------------------------------------------------------------------------------*\
+		UPDATE ERROR COUNTS
+	\*----------------------------------------------------------------------------------------------------------------*/
+	updateCounts();
+	function updateCounts()
+	{
+		$(".imageMissingAlt").text("Images missing alt text: " + imageMissingAltCount);
+		$(".imageEmptyAlt").text("Images empty alt text: " + imageEmptyAltCount);
+	}
+	
+	
+	
+	
+	
+	/*----------------------------------------------------------------------------------------------------------------*\
+		TOGGLE ERRORS
+	\*----------------------------------------------------------------------------------------------------------------*/
 	function toggleError(element)
 	{
-		$(element).toggle(
-			function () {
-				$(this).css({"outline": "red"});
-				$(this).addClass("debuggerError");
-			},
-			function () {
-				$(this).css({"outline": "none"});
-				$(this).removeClass("debuggerError");
-			}
-		);
+		if($(element).hasClass("debuggerError"))
+		{
+			$(element).css({"outline": "none"});
+			$(element).removeClass("debuggerError");
+		} else {
+			$(element).css({"outline": "5px solid red"});
+			$(element).addClass("debuggerError");
+		}
 	}
 	
-	//toggle warning on an element
 	function toggleWarning(element)
-	{
-		$('img[alt=""]').toggle(
-			function () {
-				$(this).css({"outline": "yellow"});
-				$(this).addClass("debuggerWarning");
-			},
-			function () {
-				$(this).css({"outline": "none"});
-				$(this).removeClass("debuggerWarning");
-			}
-		);
+	{		
+		if($(element).hasClass("debuggerWarning"))
+		{
+			$(element).css({"outline": "none"});
+			$(element).removeClass("debuggerWarning");
+		} else {
+			$(element).css({"outline": "5px solid yellow"});
+			$(element).addClass("debuggerWarning");
+		}
 	}
 	
 	
 	
 	
 	
+	/*----------------------------------------------------------------------------------------------------------------*\
+		ERROR CHECKS
+	\*----------------------------------------------------------------------------------------------------------------*/
 	//highlight images with missing alt attribute
 	function imageMissingAlt()
 	{
 		$('img:not([alt])').each(function(index) {
 			toggleError($(this));
+			
+			imageMissingAltCount++;
 		});
 	}
 	
@@ -55,6 +128,8 @@ Debug = function(element)
 	{
 		$('img[alt=""]').each(function(index) {
 			toggleWarning($(this));
+			
+			imageEmptyAltCount++;
 		});
 	}
 };
